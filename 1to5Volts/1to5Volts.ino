747f85c3fccfd3122d3411e51453df741e0f2d65
Md Imran Hasan <eeeimranhasan@gmail.com>
2024-05-20 17:39:05 +0600

    DAC test code basic

---
+++ 1to5Volts/1to5Volts.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+Adafruit_MCP4725 dac;
+// Set this value to 9, 8, 7, 6 or 5 to adjust the resolution
+#define DAC_RESOLUTION    (9)
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("MCP4725A1 Test");
+  //MCP4725A1 address is 0x60 (default); 
+  // MCP4725A1 address is 0x63 (ADDR pin tied to VCC) 
+  // MCP4725A1 address is 0x60 (ADDR pin tied to GND) 
+  dac.begin(0x60); //I have my ADDR pin connected to GND so address is 0x60
+}
+
+void loop(void) {      
+  dac.setVoltage((0.1*4095)/5, false);        //Set voltage to 1V
+  delay(2000);
+  dac.setVoltage((0.2*4095)/5, false);        //Set voltage to 2V
+  delay(2000);
+  dac.setVoltage((0.3*4095)/5, false);        //Set voltage to 3V
+  delay(2000);
+  dac.setVoltage((0.4*4095)/5, false);        //Set voltage to 4V
+  delay(2000);
+ dac.setVoltage((0.5*4095)/5, false);            //Set voltage to 5V or (Vcc)
+  delay(2000); 
+}
